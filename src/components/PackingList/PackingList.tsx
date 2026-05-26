66f6049e189c5444a981d7601c4c5cf5c609d729
Clemens Sietas <siecle@gmx.de>
2026-05-26 15:51:22 +0200

    Initial items created

--- src/components/PackingList/PackingList.tsx
+++ src/components/PackingList/PackingList.tsx
@@ -1,7 +1,20 @@
 import './PackingList.css';
 
 const PackingList = () => {
-  return <div className="list">LIST</div>;
+  const initalItems = [
+    { id: 1, description: 'Passports', quantity: 2, packed: false },
+    { id: 2, description: 'Socks', quantity: 12, packed: true },
+    { id: 3, description: 'Chargers', quantity: 3, packed: false },
+  ];
+  return (
+    <div className="list">
+      <ul>
+        {initalItems.map((item) => (
+          <li key={item.id}>{item.description}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PackingList;
