85d6751bcc156042c405dde2e39aeacc27b7acb9
Clemens Sietas <siecle@gmx.de>
2026-05-26 18:25:27 +0200

    Form component: select, input, button

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,9 +2,18 @@ import './Form.css';
 
 const Form: React.FC = () => {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select name="" id="">
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 };
 
