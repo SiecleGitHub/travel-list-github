07c13d883c8a7eb6bcf5ed31dc45d453f216437d
Clemens Sietas <siecle@gmx.de>
2026-05-26 19:27:46 +0200

    Form component: create a new item

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,21 +1,37 @@
+import { useState } from 'react';
 import './Form.css';
 
 const Form: React.FC = () => {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   const handleEvent = (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!description.trim()) return;
+
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false,
+    };
+
+    setDescription('');
+    setQuantity(1);
   };
 
   return (
     <form className="add-form" onSubmit={handleEvent}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select name="" id="">
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option key={num} value={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)} />
       <button>Add</button>
     </form>
   );
