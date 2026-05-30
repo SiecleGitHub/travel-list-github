13a8445771f19699f7ccd9aca550c05b6459f848
Clemens Sietas <siecle@gmx.de>
2026-05-30 19:21:59 +0200

    Conditional messages added, one with early return

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -2,13 +2,24 @@ import './Stats.css';
 import { ItemModel } from '../../types/item-model';
 
 const Stats = ({ items }: { items: ItemModel[] }) => {
+  // Early return for empty list
+  if (items.length === 0) {
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+  }
+  // No need to calculate stats earlier than here
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
       <em>
-        You have {numItems} items on your list, and you already packed {numPacked} ({percentage}%)
+        {percentage === 100
+          ? 'You got everything! Ready to go ✈️'
+          : `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   );
